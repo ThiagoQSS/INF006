1f1a3d4c378d581d268e7ad2fa311c63025edf21
ThiagoQSS <tqss2759@gmail.com>
2025-01-27 21:48:33 +0000

    Quase pronto

--- trabalho3/Q1 - ThiagoSacramento/L3Q1.c
+++ trabalho3/Q1 - ThiagoSacramento/L3Q1.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#define TAM 10000
+
 
 typedef struct no {
     int valor;
@@ -41,7 +43,7 @@ int inserirEmArv(Raiz * arv, int valor) //vai retornar a altura
     while (1)
     {
         altura++;
-        if (valor > aux->valor) 
+        if (valor >= aux->valor) 
         {
             if(aux->direita != NULL) 
                 aux = aux->direita;
@@ -50,7 +52,7 @@ int inserirEmArv(Raiz * arv, int valor) //vai retornar a altura
                 return altura;
             }
         } 
-        else if(valor < aux->valor) 
+        else 
         {
             if(aux->esquerda != NULL) 
                 aux = aux->esquerda;
@@ -60,6 +62,45 @@ int inserirEmArv(Raiz * arv, int valor) //vai retornar a altura
             }
         }
     }
+
+}
+
+int buscarMaiorElemento(Raiz * arv) {
+    if(arv->inicio == NULL)
+        return -1;
+    //
+    No * aux = arv->inicio;
+    while(aux->direita)
+        aux = aux->direita;
+    //
+    return aux->valor;
+}
+
+int buscarAlturaDoMaiorElemento(Raiz * arv) {
+    if(arv->inicio == NULL)
+        return -1;
+    //
+    No * aux = arv->inicio;
+    int altura = 0;
+    while(aux->direita) {
+        altura++;
+        aux = aux->direita;
+    }
+    //
+    return altura;
+}
+
+int buscarPredecessorDoMaiorElemento(Raiz * arv) {
+    No * aux = arv->inicio;
+    No * sacrifice = arv->inicio;
+    if(sacrifice->direita)
+        sacrifice = sacrifice->direita;
+    while(sacrifice->direita) {
+        sacrifice = sacrifice->direita;
+        aux = aux->direita;
+    }
+    //
+    return aux->valor;
 }
 
 
@@ -67,16 +108,60 @@ int main ()
 {
     system("cls||clear");
 
-    Raiz * arvore = initArv(); 
+    FILE * entrada;
+    FILE * saida;
 
-    printf("%d\n", inserirEmArv(arvore, 5));
-    printf("%d\n", inserirEmArv(arvore, 8));
-    printf("%d\n", inserirEmArv(arvore, -1));
-    printf("%d\n", inserirEmArv(arvore, 4));
-    printf("%d\n", inserirEmArv(arvore, 6));
-    printf("%d\n", inserirEmArv(arvore, 9));
-    printf("%d\n", inserirEmArv(arvore, 11));
-    printf("%d\n", inserirEmArv(arvore, 15));
+    char string[TAM];
+    char separador[] = " ";
+    char * chunk;
 
+    entrada = fopen("L2Q1.in", "r");
+    saida = fopen("L2Q1.out", "w");
+
+    if (entrada == NULL || saida == NULL) {
+        printf("Erro ao abrir arquivos!");
+        return 0;
+    }
+
+    int isFirst = 0;
+    int max;
+    int pred;
+    int alt;
+
+    while (fgets(string, TAM, entrada)) {
+        Raiz * arvore = initArv();
+        isFirst = 1;
+        chunk = strtok(string, separador);
+
+        while(chunk != NULL) {
+          
+            if(isFirst) {
+                fprintf(saida, "%d", inserirEmArv(arvore, atoi(chunk)));
+                isFirst = 0;
+            }
+            else 
+                fprintf(saida, " %d", inserirEmArv(arvore, atoi(chunk)));
+            //
+            printf("%s ", chunk);
+
+            chunk = strtok(NULL, separador);
+        }
+
+        alt = buscarAlturaDoMaiorElemento(arvore);
+        max = buscarMaiorElemento(arvore);
+        pred = buscarPredecessorDoMaiorElemento(arvore);
+
+        fprintf(saida, " max %d", max);
+        fprintf(saida, " alt %d", alt);
+
+        if(pred != arvore->inicio->valor)
+            fprintf(saida, " pred %d", pred);
+        else
+            fprintf(saida, " pred NaN");
+        //
+
+        printf("\n");
+        fprintf(saida, "\n");
+    }
 
 }
