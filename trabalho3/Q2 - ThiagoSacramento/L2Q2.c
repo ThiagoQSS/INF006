4730d03c053467ae9cef53519715f24972989000
ThiagoQSS <tqss2759@gmail.com>
2025-01-31 21:40:49 +0000

    Questão 2 finalizada

--- trabalho3/Q2 - ThiagoSacramento/L2Q2.c
+++ trabalho3/Q2 - ThiagoSacramento/L2Q2.c
@@ -90,7 +90,7 @@ No * buscarElemento(Raiz * arv, int valor)
 }
 
 
-No * buscarSucessorimediato(No * elemento) 
+No * buscarSucessorimediato(No * elemento, Raiz * arv) 
 {
     if(elemento == NULL)
         return NULL;
@@ -104,11 +104,14 @@ No * buscarSucessorimediato(No * elemento)
     } 
     else if (aux->parent != NULL){
         parent = aux->parent;
-        while(parent->direita == aux) {
+        while(parent->direita == aux && parent->parent != NULL) {
             aux = parent;
             parent = aux->parent;
         }
-        return parent;
+        if (parent->valor > aux->valor)
+            return parent;
+        else
+            return NULL;
     }
     else
         return NULL;
@@ -129,12 +132,12 @@ int somarSubArvore(No * galho)
     if (galho == NULL)
         return 0;
     int valor = galho->valor;
+    if (galho->direita == NULL && galho->esquerda == NULL) 
+        return valor;
     if (galho->direita == NULL)
         return valor + somarSubArvore(galho->esquerda);
     if (galho->esquerda == NULL)
         return valor + somarSubArvore(galho->direita);
-    if (galho->direita == NULL && galho->esquerda == NULL) 
-        return valor;
     return valor + somarSubArvore(galho->esquerda) + somarSubArvore(galho->direita);
 }
 
@@ -143,9 +146,9 @@ int calcularDiffEsqDir(No * galho) {
     if (galho->direita == NULL && galho->esquerda == NULL) 
         return 0;
     if (galho->esquerda == NULL)
-        return 0 - somarSubArvore(galho->direita);
+        return somarSubArvore(galho->direita);
     if (galho->direita == NULL)
-        return somarSubArvore(galho->esquerda);
+        return 0 - somarSubArvore(galho->esquerda);
     return somarSubArvore(galho->direita) - somarSubArvore(galho->esquerda);
 }
 
@@ -155,7 +158,50 @@ int main()
 
     system("clear||cls");
 
-    Raiz * arvore = initArv();
+    FILE * entrada = fopen("L2Q2.in", "r");
+    FILE * saida = fopen("L2Q2.out", "w");
 
+    if(entrada == NULL || saida == NULL) {
+        printf("Erro ao abrir Arquivos!!!");
+        return 0;
+    }
+
+    char string[TAM];
+    char separador[] = " ";
+    char * chunk;
+
+    int isFirst = 1;
+    int inicioLinha = 1;
     
+    while (fgets(string, TAM, entrada)) {
+        inicioLinha = 1;
+        Raiz * arvore = initArv();
+        if(!isFirst) 
+            fprintf(saida, "\n"); 
+        else
+            isFirst = 0;
+        //
+
+        chunk = strtok(string, separador);
+
+        while (chunk) {
+            printf("%d\n", inserirEmArv(arvore, atoi(chunk)));
+            printf("<%s>\n", chunk);
+            chunk = strtok(NULL, separador);
+        }
+
+        No * atual = buscarMinimo(arvore); 
+        while (atual != NULL) {
+            if(inicioLinha == 1) {
+                fprintf(saida, "%d", atual->valor);
+                inicioLinha = 0;
+            } else {
+                fprintf(saida, " %d", atual->valor);
+            }
+            int somaDif = calcularDiffEsqDir(atual);
+            fprintf(saida, " (%d)", somaDif);
+
+            atual = buscarSucessorimediato(atual, arvore);
+        }
+    }
 }
